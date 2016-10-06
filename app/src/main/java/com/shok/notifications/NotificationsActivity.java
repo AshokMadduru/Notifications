497e7283a4e915e1d708022c9de324c47cc4e99f
ashokm <ashok.madduru@riktamtech.com>
2016-10-06 18:26:36 +0530

    Add Basic notification

--- app/src/main/java/com/shok/notifications/NotificationsActivity.java
+++ app/src/main/java/com/shok/notifications/NotificationsActivity.java
@@ -1,7 +1,10 @@
 package com.shok.notifications;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.NotificationManager;
+import android.content.Context;
 import android.os.Bundle;
+import android.support.v4.app.NotificationCompat;
+import android.support.v7.app.AppCompatActivity;
 
 public class NotificationsActivity extends AppCompatActivity {
 
@@ -9,5 +12,22 @@ public class NotificationsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_notifications);
+
+        createNotification();
     }
+
+    private void createNotification() {
+        NotificationManager mNotificationManager =
+                (NotificationManager) this.getSystemService(Context.NOTIFICATION_SERVICE);
+
+        NotificationCompat.Builder mBuilder =
+                new NotificationCompat.Builder(this)
+                        .setSmallIcon(R.mipmap.ic_launcher)
+                        .setContentTitle(getResources().getString(R.string.notification_title))
+                        .setContentText(getResources().getString(R.string.notification_desc));
+
+        mNotificationManager.notify(1, mBuilder.build());
+    }
+
+
 }
