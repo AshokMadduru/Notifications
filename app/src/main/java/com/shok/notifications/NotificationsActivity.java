fc8454942c3635ddcc5b7a393d39c7ab84a9f54c
ashokm <ashok.madduru@riktamtech.com>
2016-10-06 18:32:37 +0530

    Add onclick functionality to notification. Redirect to notifications screen upon click

--- app/src/main/java/com/shok/notifications/NotificationsActivity.java
+++ app/src/main/java/com/shok/notifications/NotificationsActivity.java
@@ -1,7 +1,9 @@
 package com.shok.notifications;
 
 import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.NotificationCompat;
 import android.support.v7.app.AppCompatActivity;
@@ -25,6 +27,16 @@ public class NotificationsActivity extends AppCompatActivity {
                         .setSmallIcon(R.mipmap.ic_launcher)
                         .setContentTitle(getResources().getString(R.string.notification_title))
                         .setContentText(getResources().getString(R.string.notification_desc));
+
+        Intent resultIntent = new Intent(this, NotificationsActivity.class);
+        PendingIntent resultPendingIntent =
+                PendingIntent.getActivity(
+                        this,
+                        0,
+                        resultIntent,
+                        PendingIntent.FLAG_UPDATE_CURRENT
+                );
+        mBuilder.setContentIntent(resultPendingIntent);
 
         mNotificationManager.notify(1, mBuilder.build());
     }
