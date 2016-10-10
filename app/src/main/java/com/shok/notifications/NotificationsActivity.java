7a6c701650096406403c9664b591d13fd3e69323
ashokm <ashokgirihari@gmail.com>
2016-10-10 12:16:42 +0530

    add notification sound

--- app/src/main/java/com/shok/notifications/NotificationsActivity.java
+++ app/src/main/java/com/shok/notifications/NotificationsActivity.java
@@ -4,6 +4,8 @@ import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
+import android.media.RingtoneManager;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.NotificationCompat;
 import android.support.v7.app.AppCompatActivity;
@@ -22,11 +24,14 @@ public class NotificationsActivity extends AppCompatActivity {
         NotificationManager mNotificationManager =
                 (NotificationManager) this.getSystemService(Context.NOTIFICATION_SERVICE);
 
+        Uri defaultSoundUri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
+
         NotificationCompat.Builder mBuilder =
                 new NotificationCompat.Builder(this)
                         .setSmallIcon(R.mipmap.ic_launcher)
                         .setContentTitle(getResources().getString(R.string.notification_title))
-                        .setContentText(getResources().getString(R.string.notification_desc));
+                        .setContentText(getResources().getString(R.string.notification_desc))
+                        .setSound(defaultSoundUri);
 
         Intent resultIntent = new Intent(this, NotificationsActivity.class);
         PendingIntent resultPendingIntent =
